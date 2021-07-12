7698b910775a9de831a9855bfc376123fd60e3b7
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-07-12 14:05:04 +0200

    split tests

--- asyncapi/v2/decode_test.go
+++ asyncapi/v2/decode_test.go
@@ -8,43 +8,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//nolint:funlen,misspell
-func TestDecode(t *testing.T) {
-	tests := []struct {
-		name          string
-		doc           []byte
-		dst           interface{}
-		expectedError error
-		check         func(t *testing.T, dst interface{})
-	}{
-		{
-			name: "from yaml file example-kafka.yaml",
-			doc:  []byte("testdata/example-kafka.yaml"),
-			dst:  &Document{},
-			check: func(t *testing.T, dst interface{}) {
-				require.IsType(t, (*Document)(nil), dst)
-				doc := dst.(*Document)
-				require.Len(t, doc.Servers(), 1)
-				s := doc.Servers()[0]
+func TestDecodeFromFile(t *testing.T) {
+	doc := new(Document)
+	require.NoError(t, Decode([]byte("testdata/example-kafka.yaml"), doc))
 
-				assert.True(t, s.HasName())
-				assert.Equal(t, "test", s.Name())
-				assert.True(t, s.HasDescription())
-				assert.Equal(t, "Test broker", s.Description())
-				assert.True(t, s.HasProtocol())
-				assert.Equal(t, "kafka-secure", s.Protocol())
-				assert.True(t, s.HasURL())
-				assert.Equal(t, "localhost:9092", s.URL())
-				assert.True(t, s.HasExtension("x-eventgateway-listener"))
-				assert.Equal(t, "proxy:28002", s.Extension("x-eventgateway-listener"))
-				assert.True(t, s.HasExtension("x-eventgateway-dial-mapping"))
-				assert.Equal(t, "broker:9092", s.Extension("x-eventgateway-dial-mapping"))
-				assert.Empty(t, s.Variables())
-			},
-		},
-		{
-			name: "from plain text yaml",
-			doc: []byte(`
+	require.Len(t, doc.Servers(), 1)
+	s := doc.Servers()[0]
+
+	assert.True(t, s.HasName())
+	assert.Equal(t, "test", s.Name())
+	assert.True(t, s.HasDescription())
+	assert.Equal(t, "Test broker", s.Description())
+	assert.True(t, s.HasProtocol())
+	assert.Equal(t, "kafka-secure", s.Protocol())
+	assert.True(t, s.HasURL())
+	assert.Equal(t, "localhost:9092", s.URL())
+	assert.True(t, s.HasExtension("x-eventgateway-listener"))
+	assert.Equal(t, "proxy:28002", s.Extension("x-eventgateway-listener"))
+	assert.True(t, s.HasExtension("x-eventgateway-dial-mapping"))
+	assert.Equal(t, "broker:9092", s.Extension("x-eventgateway-dial-mapping"))
+	assert.Empty(t, s.Variables())
+}
+
+//nolint:misspell
+func TestDecodeFromPlainText(t *testing.T) {
+	raw := []byte(`
 asyncapi: '2.0.0'
 info:
   title: Streetlights API
@@ -80,39 +68,22 @@ channels:
             sentAt:
               type: string
               format: date-time
-              description: Date and time when the message was sent.`),
-			dst: &Document{},
-			check: func(t *testing.T, dst interface{}) {
-				require.IsType(t, (*Document)(nil), dst)
-				doc := dst.(*Document)
-				require.Len(t, doc.Servers(), 1)
-				s := doc.Servers()[0]
+              description: Date and time when the message was sent.`)
 
-				assert.True(t, s.HasName())
-				assert.Equal(t, "mosquitto", s.Name())
-				assert.False(t, s.HasDescription())
-				assert.True(t, s.HasProtocol())
-				assert.Equal(t, "mqtt", s.Protocol())
-				assert.True(t, s.HasURL())
-				assert.Equal(t, "mqtt://test.mosquitto.org", s.URL())
-				assert.False(t, s.HasExtension("x-eventgateway-listener"))
-				assert.False(t, s.HasExtension("x-eventgateway-dial-mapping"))
-				assert.Empty(t, s.Variables())
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			err := Decode(test.doc, test.dst)
-			if test.expectedError != nil {
-				assert.EqualError(t, err, test.expectedError.Error())
-			} else {
-				assert.NoError(t, err)
-			}
+	doc := new(Document)
+	require.NoError(t, Decode(raw, doc))
 
-			if test.check != nil {
-				test.check(t, test.dst)
-			}
-		})
-	}
+	require.Len(t, doc.Servers(), 1)
+	s := doc.Servers()[0]
+
+	assert.True(t, s.HasName())
+	assert.Equal(t, "mosquitto", s.Name())
+	assert.False(t, s.HasDescription())
+	assert.True(t, s.HasProtocol())
+	assert.Equal(t, "mqtt", s.Protocol())
+	assert.True(t, s.HasURL())
+	assert.Equal(t, "mqtt://test.mosquitto.org", s.URL())
+	assert.False(t, s.HasExtension("x-eventgateway-listener"))
+	assert.False(t, s.HasExtension("x-eventgateway-dial-mapping"))
+	assert.Empty(t, s.Variables())
 }
