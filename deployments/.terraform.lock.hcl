754695326707ef4c2792eb7f3cb2dd6246e0ebf4
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-08-30 17:01:48 +0200

    chore(infra): add terraform config files for Kafka test server

---
+++ deployments/.terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/aiven/aiven" {
+  version     = "2.1.19"
+  constraints = "2.1.19"
+  hashes = [
+    "h1:Q8dIAuqK+Sv0wHHCPyC74C7GW8ffBiRUsZ2GracbhP0=",
+    "zh:1c7aaa3f12271efb11644b1e16faa78dae7808ee135d0111085849f7763d6d70",
+    "zh:2588f616a1845100818a2625f70b3ca9ed50114e8e63f867e2da944b5be4c9cb",
+    "zh:33058f8ab3037f20bf94dc11c8b075ee12343d945b1a1d3626da3fcafaff2ea5",
+    "zh:4966f57a0b2af14c9eefc0d3d2de8f9a164b375b7769675f16da811396d61765",
+    "zh:59078056d28f6112cb411e63a49463184f5ab4ec98003407d9552f6926256253",
+    "zh:5ed3a1cdd00cc3170d8774c838879d94b04819bec8317b1de89589860adbd084",
+    "zh:77733c373b28fdc0c5aa6b67bd81a87868714a1093db33c1bfd017decab1433f",
+    "zh:a2fe5f1ffcf20df3904b0f1b05403acfb93f35ca9184521751293f9a14fefda6",
+    "zh:af5091229b77ab5be680e0e91221d1ee191663c656fb27301e146fee2c6b2dc3",
+    "zh:b36d6de3211728f680feab7962f1dc9e58f4a1ba2014ac4675a5ea5169d60b09",
+    "zh:d0ce4873c1bfb8a097ad0596a838d3c00b7f18d8ee86fcf403bd5ff173697cd3",
+    "zh:f86b617ad29fd66166edce7f640cfbed0cfc498d71284458c96250a05b9be721",
+    "zh:ff42caef100af7d6ea6e6934264dfd29f39ef6334381256eed710ea309d9af5d",
+  ]
+}
---
+++ deployments/main.tf
@@ -0,0 +1,95 @@
+variable "aiven_api_token" {
+  type = string
+}
+
+variable "aiven_project_name" {
+  type = string
+}
+
+variable "aiven_service_name" {
+  type = string
+}
+
+variable "aiven_service_plan" {
+  type = string
+}
+
+variable "aiven_kafka_cloud_name" {
+  type = string
+}
+
+variable "aiven_kafka_topic_name" {
+  type = string
+}
+
+variable "aiven_kafka_username" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    aiven = {
+      source  = "aiven/aiven"
+      version = "2.1.19"
+    }
+  }
+}
+
+provider "aiven" {
+  api_token = var.aiven_api_token
+}
+
+data "aiven_project" "asyncapi-project" {
+  project = var.aiven_project_name
+}
+
+# Kafka service
+resource "aiven_kafka" "asyncapi-event-gateway-kafka-service" {
+  project                 = data.aiven_project.asyncapi-project.project
+  cloud_name              = var.aiven_kafka_cloud_name
+  plan                    = var.aiven_service_plan
+  service_name            = var.aiven_service_name
+  maintenance_window_dow  = "friday"
+  maintenance_window_time = "07:00:00"
+  termination_protection = true
+  kafka_user_config {
+    kafka_version = "2.7"
+  }
+}
+
+# Topic for Kafka
+resource "aiven_kafka_topic" "asyncapi-event-gateway-kafka-topic" {
+  project      = data.aiven_project.asyncapi-project.project
+  service_name = aiven_kafka.asyncapi-event-gateway-kafka-service.service_name
+  topic_name   = var.aiven_kafka_topic_name
+  partitions   = 1
+  replication  = 2
+  config {
+    retention_ms = 60000
+  }
+}
+
+# User for Kafka
+resource "aiven_service_user" "asyncapi-event-gateway-kafka-user" {
+  project      = data.aiven_project.asyncapi-project.project
+  service_name = aiven_kafka.asyncapi-event-gateway-kafka-service.service_name
+  username     = var.aiven_kafka_username
+}
+
+# ACL for Kafka
+resource "aiven_kafka_acl" "asyncapi-event-gateway-read-acl" {
+  project      = data.aiven_project.asyncapi-project.project
+  service_name = aiven_kafka.asyncapi-event-gateway-kafka-service.service_name
+  username     = aiven_service_user.asyncapi-event-gateway-kafka-user.username
+  permission   = "read"
+  topic        = aiven_kafka_topic.asyncapi-event-gateway-kafka-topic.topic_name
+}
+
+# ACL for Kafka
+resource "aiven_kafka_acl" "asyncapi-event-gateway-write-acl" {
+  project      = data.aiven_project.asyncapi-project.project
+  service_name = aiven_kafka.asyncapi-event-gateway-kafka-service.service_name
+  username     = aiven_service_user.asyncapi-event-gateway-kafka-user.username
+  permission   = "write"
+  topic        = aiven_kafka_topic.asyncapi-event-gateway-kafka-topic.topic_name
+}
