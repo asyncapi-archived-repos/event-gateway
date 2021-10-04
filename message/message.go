21358e8fa4c2901af50ef3fffb899677f351b467
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-10-04 13:04:46 +0200

    clarify metadata keys prefix

--- message/message.go
+++ message/message.go
@@ -5,6 +5,9 @@ import (
 	watermillmessage "github.com/ThreeDotsLabs/watermill/message"
 )
 
+// The following constants are the keys on a watermill.Message Metadata where we store valuable and needed domain metadata.
+// All contain the prefix `_asyncapi_eg_` so they can be unique-ish and human-readable.
+// As a note: The term `eg` is a short version of Event-Gateway.
 const (
 	// MetadataChannel is the key used for storing the Channel in the message Metadata.
 	MetadataChannel = "_asyncapi_eg_channel"
